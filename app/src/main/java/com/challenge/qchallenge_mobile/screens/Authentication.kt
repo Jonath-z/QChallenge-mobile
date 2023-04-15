29572eab7ccd0c64792f107b7148c2f471d439f3
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-15 13:08:50 +0200

    refactor: remove unused packages

--- app/src/main/java/com/challenge/qchallenge_mobile/screens/Authentication.kt
+++ app/src/main/java/com/challenge/qchallenge_mobile/screens/Authentication.kt
@@ -1,34 +1,22 @@
 package com.challenge.qchallenge_mobile.screens
 
 
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.challenge.qchallenge_mobile.R
-import com.challenge.qchallenge_mobile.ui.TranlateYImage
 import com.challenge.qchallenge_mobile.ui.buttons.PrimaryFilledButton
 import com.challenge.qchallenge_mobile.ui.inputs.TextInput
 import com.challenge.qchallenge_mobile.ui.theme.PrimaryYellow
-import com.challenge.qchallenge_mobile.ui.theme.QChallengemobileTheme
 
 @Composable
 fun Authentication(){
